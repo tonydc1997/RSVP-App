31cc5dd7fb654c9b82c4ed49d312c1480e867b6a
Anthony Curtis <tonydc1997@gmail.com>
2018-03-26 08:23:00 -0400

    Register names

---
+++ app.js
@@ -0,0 +1,11 @@
+const form = document.getElementById('registrar');
+const input = form.querySelector('input');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const text = input.value;
+  const ul = document.getElementById('invitedList');
+  const li = document.createElement('li');
+  li.textContent = text;
+  ul.appendChild(li);
+}); 
