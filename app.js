5f213125aa493b1385b7760dc57ae52a17ddb43f
Anthony Curtis <tonydc1997@gmail.com>
2018-03-26 08:29:39 -0400

    Register names

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const input = form.querySelector('input');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const text = input.value;
+  input.value = '';
   const ul = document.getElementById('invitedList');
   const li = document.createElement('li');
   li.textContent = text;
